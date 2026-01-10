396cc20eef9467cbf7f1a5eae544281d3a9cb1fd
Michel <michel.c.araujo@gmail.com>
2026-01-10 02:41:25 -0300

    fix: proper CORS preflight handling

--- src/routes/api/v1/generate-data.routes.js
+++ src/routes/api/v1/generate-data.routes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { generatePresellData } = require('../../../controllers/presell.controller');
-const { verifyWorker, verifyUser } = require('../../../middlewares/auth');
+const { generatePresellData } = require("../../../controllers/presell.controller");
+const { verifyWorker, verifyUser } = require("../../../middlewares/auth");
+
+// 🔓 LIBERA PREFLIGHT
+router.options("/generate-data", (req, res) => {
+  res.sendStatus(204);
+});
 
 router.post(
-  '/generate-data',
+  "/generate-data",
   verifyWorker,
   verifyUser,
   generatePresellData
