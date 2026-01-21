1ef6d41495bcebaae0e98272dc13a63ba3aa161e
unknown <DESPACHADOR@PD0504.cmdo.policiamilitar.sp.gov.br>
2026-01-21 02:04:49 -0300

    feat: add Cakto webhook

--- src/routes/api/v1/webhook.routes.js
+++ src/routes/api/v1/webhook.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const kiwifyWebhook = require("../../../controllers/webhook.kiwify");
+const caktoWebhook = require("../../../controllers/webhook.cakto");
 
 const router = express.Router();
 
 router.post("/webhooks/kiwify", express.json(), kiwifyWebhook);
+router.post("/webhooks/cakto", express.json(), caktoWebhook);
 
 module.exports = router;
